d2db145e555bc0a9568c7d76f94eccaf59266f58
Sérgio Brito <71470010+Sergio-BR1@users.noreply.github.com>
2023-06-26 00:16:05 -0300

    Update Processos.js
    corrected call to Round Robin function

--- frontend/trabalho-so/src/components/Processos.js
+++ frontend/trabalho-so/src/components/Processos.js
@@ -73,7 +73,7 @@ const callSJF = (e) => {
 
 const callRR = (e) => {
   e.preventDefault();
-  console.log(escalonamentoRR(processos, 2));
+  console.log(RoundRobin(processos, 2));
   setProcessos([]);
 }
 
