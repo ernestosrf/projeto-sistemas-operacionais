0462e6f0f9eca8003e844f38e5c6a28d58662039
Ernesto Reis <ernestosrf98@gmail.com>
2023-07-09 19:41:00 -0300

    remove comment

--- frontend/trabalho-so/src/App.js
+++ frontend/trabalho-so/src/App.js
@@ -5,13 +5,10 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 // Pages
 import Home from "./Pages/Home/Home";
-import Result from "./Pages/Result/Result";
 
 
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-// import {useState} from 'react';
-// import fifo from './data/functions/fifo';
 
 function App() {
   return (
@@ -21,16 +18,10 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/Result" element={<Result />} />
           </Routes>
         </div>
         <Footer />
       </BrowserRouter>
-      {/* <Navbar />
-        <div className="container">
-          <Processos/>
-        </div>
-      <Footer /> */}
     </div>
   );
 }
